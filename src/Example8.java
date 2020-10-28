ead461a25868c74b51a852d8e9a91ff6cda69080
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2020-10-28 21:09:53 +0100

    Example8

---
+++ src/Example8.java
@@ -0,0 +1,35 @@
+public class Example8 {
+
+    public static void main(String[] args) {
+
+        // 1 ... 9
+        // 50 ... 100
+        // startWert ... EndWert
+
+        /*int startWert = 2;
+        int endWert = 9;
+        while (startWert <= endWert){
+
+            System.out.println(startWert);
+            startWert++;
+        }*/
+
+        long zahl = 2105;
+        for (int i = 2; i <= 9; i++) {
+
+        }
+
+        /*
+        2105 * 2 = 4210
+        4210 * 3 = 12630
+        12630 * 4 = ....
+        ...
+        xx * 9 = y
+
+        y / 2 = xx2
+        y / 3 = xx3
+        ...
+        y / 9 = 2105
+         */
+    }
+}
