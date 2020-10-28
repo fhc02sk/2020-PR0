a4647ad9e916c253ade5651c72148b5cf32caa49
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2020-10-28 17:55:44 +0100

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputInt = scanner.nextInt();
+        String inputString = scanner.next();
+    }
+}
