ffcf279c230ee5464e7e26687adf12a5823c738b
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2020-10-28 20:08:11 +0100

    Example5

--- src/Example4.java
+++ src/Example4.java
@@ -1,20 +1,29 @@
 public class Example4 {
     public static void main(String[] args) {
 
-        double km = 12.7;
+        double km = 12.71;
 
         System.out.println("Kilometer: " + km);
 
-        // Methode
-
-        //double miles = 20.32;
-
+        // Methodenaufruf
+        double miles;
+        miles = km2miles(km);
         System.out.println("km = " + km + " umgerechnet in Meilen sind: " + miles);
 
-        // km = 12.7 umgerechnet in Meilen sind das 20.32
+        double km_neu = miles2km(miles);
+        System.out.println("miles = " + miles + " umgerechnet in Kilometer sind: " + km_neu);
+
+        System.out.println("miles = " + miles + " umgerechnet in Kilometer sind: "
+                + miles2km(miles));
+
     }
 
-    public static double km2miles(double km){
+    public static double km2miles(double kmA){
+        double erg = kmA / 1.6;
+        return erg;
+    }
 
+    public static double miles2km(double miles){
+        return (miles * 1.6);
     }
 }
---
+++ src/Example5.java
@@ -0,0 +1,33 @@
+public class Example5 {
+    public static void main(String[] args) {
+
+        boolean isCampus02 = true;
+        if (isCampus02){
+            System.out.println("Bin am Campus");
+        }
+
+        boolean isCoronaTimes = true;
+        if (isCoronaTimes && isCampus02){
+            System.out.println("Vorlesung via Teams");
+        }
+        else{
+            System.out.println("Vorlesung auf der Wifi");
+        }
+
+        // da passiert irgendwas
+        // jetzt haben wir april
+        isCoronaTimes = false;
+        if (isCoronaTimes == true && isCampus02 == true){
+            System.out.println("Vorlesung via Teams");
+        }
+        else{
+            System.out.println("Vorlesung auf der Wifi");
+        }
+
+        int tankfuellung = 50;
+        if (tankfuellung < 10){
+            System.out.println("bitte tanken");
+        }
+
+    }
+}
